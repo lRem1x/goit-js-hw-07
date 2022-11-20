05cbf6a6fb585008072bf7461c9a8fee3de37740
Roman Balakleec <lrem1x777@gmail.com>
2022-11-21 00:55:36 +0200

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const gallerySet = createGalleryset(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', gallerySet);
+
+galleryRef.addEventListener('click', onAddItemsClick);
+
+function createGalleryset(items =[]) {
+    return items.map((item) => 
+    `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`).join("");
+}
+
+function onAddItemsClick(evt) {
+    evt.preventDefault();
+}
+
+let gallery = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250,});
